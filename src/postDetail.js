34eee7c07fdd92ef05034b13f9e4466411edadd4
youswim <youswim96@gmail.com>
2024-02-13 23:21:17 +0900

    ✨ feat: 게시물 삭제 기능 추가

--- src/postDetail.js
+++ src/postDetail.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const PostDetail = (props) => {
     const [title, setTitle] = useState(null);
     const [content, setContent] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [responseMessage, setResponseMessage] = useState('');
     const {postId} = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -25,9 +27,29 @@ const PostDetail = (props) => {
         fetchData();
       }, []);
 
+    const deleteData = async () => {
+        try {
+            const response = await fetch("http://localhost:8080/post/" + postId, {
+                method: 'DELETE'
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error('Error deleting resource:', errorData);
+                setResponseMessage('Error deleting resource');
+              }
+        } catch (error) {
+            console.error('Error:', error);
+            setResponseMessage('Error occurred during the request');
+        }
+
+    }
+
     return(
         <div>
             {loading ? <p>Loading...</p> : <div><div>{title}</div><div>{content}</div></div>}
+            <button onClick={()=>{deleteData(); navigate("/");}}>게시물 삭제하기</button>
+            {responseMessage && <p>{responseMessage}</p>}
         </div>
     )
 }
